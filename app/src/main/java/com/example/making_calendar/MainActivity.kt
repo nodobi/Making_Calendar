386d0598013c78d58a1103ce7b51ccf06db221b3
DohyeokKim <hyeokdokim@gmail.com>
2022-12-30 19:19:17 +0900

    캘린더 표현을 위한 RecyclerView, CalendarAdapter 추가
    높이를 0dp로 하고, Adapter의 onCreateViewHolder() 함수에서 높이를
    동적으로 할당해주었따.

--- app/src/main/java/com/example/making_calendar/MainActivity.kt
+++ app/src/main/java/com/example/making_calendar/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.making_calendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.util.Log
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.making_calendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val calendarRecycler = binding.recyclerCalendar
+
+        val myGridLayout = GridLayoutManager(applicationContext, 7)
+        myGridLayout.orientation = GridLayoutManager.VERTICAL
+
+        val dataSet : MutableList<String> = mutableListOf()
+        for(i in 1..45) {
+            dataSet.add("Day$i")
+        }
+        calendarRecycler.layoutManager = myGridLayout
+
+        val adapter = CalendarAdapter(dataSet)
+        calendarRecycler.adapter = adapter
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d("hyeok", "onOptionsItemSelected")
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        Log.d("hyeok", "onContextItemSelected")
+        return super.onContextItemSelected(item)
     }
 }
